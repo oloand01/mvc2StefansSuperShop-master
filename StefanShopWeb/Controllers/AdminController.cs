9dbdbc4b8713cbd9afa94d7233ad7db128ff5add
oloand01 <olofandreasson@gmail.com>
2020-04-22 15:50:29 +0200

    adminhyperlänk och sida endast synlig samt åtkomlig för admin

--- StefanShopWeb/Controllers/AdminController.cs
+++ StefanShopWeb/Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using StefanShopWeb.ViewModels;
 
 namespace StefanShopWeb.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         private readonly ApplicationDbContext dbContext;
