d2747e5e62df282b18556c44367a6b21ad9fbab0
50964106 <i.pellarp@gmail.com>
2020-04-23 16:55:48 +0200

    made basic file upload (on temp link) that saves to local root folder

--- StefanShopWeb/Controllers/AdminController.cs
+++ StefanShopWeb/Controllers/AdminController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Net.Http.Headers;
 using StefanShopWeb.Data;
 using StefanShopWeb.ViewModels;
 
@@ -13,13 +17,14 @@ namespace StefanShopWeb.Controllers
     public class AdminController : Controller
     {
         private readonly ApplicationDbContext dbContext;
-
+        private readonly IHostingEnvironment _env;
         //test kommentar
         //test 2
 
-        public AdminController(ApplicationDbContext dbContext) //TEST3
+        public AdminController(ApplicationDbContext dbContext, IHostingEnvironment env) //TEST3
         {
             this.dbContext = dbContext;
+            _env = env;
         }
         List<MenuItem> SetupMenu(string activeAction)
         {
@@ -72,6 +77,50 @@ namespace StefanShopWeb.Controllers
             model.Categories = dbContext.Categories.Select(c =>
                 new AdminCategoryListViewModel.Category { Id = c.CategoryId, Name = c.CategoryName }).ToList();
             return View(model);
+        }
+        [HttpGet]
+        public IActionResult UploadFiles()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFiles(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            //var filePaths = new List<string>();
+            var filePath = Path.GetTempFileName();
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var uploads = Path.Combine(_env.WebRootPath, "ProductImages");
+                    var fullPath = Path.Combine(uploads, GetUniqueFileName(formFile.FileName));
+                    formFile.CopyTo(new FileStream(fullPath, FileMode.Create));
+                    // full path to file in temp location
+
+                    //var filePath = Path.Combine(_env.ContentRootPath, "ProductImages") + $@"\{newFileName}";  //we are using Temp file name just for the example. Add your own file path.
+                    //filePaths.Add(filePath);
+
+                    //using (var stream = new FileStream(filePath, FileMode.Create))
+                    //{
+                    //    await formFile.CopyToAsync(stream);
+                    //}
+                }
+            }
+
+            return Ok(new { count = files.Count, size, filePath });
+        }
+
+        private string GetUniqueFileName(string fileName)
+        {
+            fileName = Path.GetFileName(fileName);
+            return Path.GetFileNameWithoutExtension(fileName)
+                      + "_"
+                      + Guid.NewGuid().ToString().Substring(0, 4)
+                      + Path.GetExtension(fileName);
         }
 
     }
