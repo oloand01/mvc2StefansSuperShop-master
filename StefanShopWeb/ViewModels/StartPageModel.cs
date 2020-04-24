2541fba2e3d025ec7b727df65f5d9cd770db03de
mikaellettestad <mikael.lettestad@gmail.com>
2020-04-24 21:25:34 +0200

    NewsletterSubscription - Validering

--- StefanShopWeb/ViewModels/StartPageModel.cs
+++ StefanShopWeb/ViewModels/StartPageModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,5 +14,9 @@ namespace StefanShopWeb.ViewModels
             public string Name { get; set; }
         }
         public List<TrendingCategory> TrendingCategories { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [EmailAddress(ErrorMessage = "Misspelled email address")]
+        public string Email { get; set; }
     }
 }
