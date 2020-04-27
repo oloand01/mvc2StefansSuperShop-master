3429ca4482985217d5613a606600499572791a83
salleee salman <thufek@hotmail.com>
2020-04-27 14:19:19 +0200

    NewCategory funktionalitet

--- StefanShopWeb/ViewModels/AdminEditCategoryViewModel.cs
+++ StefanShopWeb/ViewModels/AdminEditCategoryViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,10 @@ namespace StefanShopWeb.ViewModels
     public class AdminEditCategoryViewModel
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        [Display(Name = "Category name")]
         public string CategoryName { get; set; }
         public string Description { get; set; }
         public string Picture { get; set; } //?? string, kanske nåt annat
