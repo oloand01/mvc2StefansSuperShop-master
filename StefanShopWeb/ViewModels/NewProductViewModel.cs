ce929811238f61dc5f8bb0199bc395d06e97351f
LisaNilsson <lisanilssonsacra@hotmail.com>
2020-04-27 17:36:11 +0200

    Added listProduct, CreateProduct, UpdateProduct

---
+++ StefanShopWeb/ViewModels/NewProductViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StefanShopWeb.ViewModels
+{
+    public class NewProductViewModel
+    {
+        public NewProduct ProductNew { get; set; }
+        public Products Product { get; set; }
+        public Suppliers Supplier { get; set; }
+        public Categories Category { get; set; }
+
+        public List<Products> ListProducts { get; set; }
+        public List<Suppliers> ListSuppliers { get; set; }
+        public List<Categories> ListCategories { get; set; }
+
+        public Suppliers SelectedSupplier { get; set; }
+        public Categories SelectedCategory { get; set; }
+
+        public NewProductViewModel()
+        {
+            Product = new Products();
+            ListProducts = new List<Products>();
+            ListSuppliers = new List<Suppliers>();
+            ListCategories = new List<Categories>();
+            //CategoryList = new List<SelectListItem>();
+            //IngredienseList = new List<SelectListItem>();
+
+
+
+        }
+    }
+}
