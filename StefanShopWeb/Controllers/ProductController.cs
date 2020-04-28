88133b7cc6f48a1193422987b1ba3e4ddb10c7e2
oloand01 <olofandreasson@gmail.com>
2020-04-28 09:13:38 +0200

    ändrade 3.1

--- StefanShopWeb/Controllers/ProductController.cs
+++ StefanShopWeb/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StefanShopWeb.Data;
+using StefanShopWeb.Models;
 using StefanShopWeb.ViewModels;
 
 namespace StefanShopWeb.Controllers
