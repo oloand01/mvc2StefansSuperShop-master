b232c527f5beb9c9c11ac29c1053303e3655ef40
salleee salman <thufek@hotmail.com>
2020-05-07 19:32:15 +0200

    fixade propsen så de matchar tabellen

--- StefanShopWeb/Models/Wishinglist.cs
+++ StefanShopWeb/Models/Wishinglist.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,12 @@ namespace StefanShopWeb.Models
 {
     public class Wishinglist
     {
+        [Key]
+        [Column("Id")]
+        public int Id { get; set; }
+        [Column("UserId")]
+        public int UserId { get; set; }
+        [Column("ProductID")]
+        public int ProductID { get; set; }
     }
 }
