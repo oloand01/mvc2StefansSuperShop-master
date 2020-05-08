c4ad8cea4f89908b7e9b5baa0df43f600a82cc4d
salleee salman <thufek@hotmail.com>
2020-05-08 14:23:41 +0200

    fixade error 500

--- StefanShopWeb/Controllers/ProductController.cs
+++ StefanShopWeb/Controllers/ProductController.cs
@@ -238,8 +238,9 @@ namespace StefanShopWeb.Controllers
             {
                 var wish = dbContext.Wishinglist.FirstOrDefault(w => w.ProductId == productid && w.UserId == user.Id);
                 dbContext.Wishinglist.Remove(wish);
-                dbContext.SaveChanges();
+                
             }
+            dbContext.SaveChanges();
             //
 
             return View();
